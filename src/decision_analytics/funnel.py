1ec9b18ca3445538ecb14bbfcdf7ee2df9ca1809
Dennis Tai <swotai@gmail.com>
2025-01-24 23:27:23 -0800

    Refactor simulation
    Refactor inputs and kpi list, and add calculation for the summary table that builds the tornado

--- src/decision_analytics/funnel.py
+++ src/decision_analytics/funnel.py
@@ -7,10 +7,18 @@ import logging
 class Funnel:
     def __init__(self, nodes_collection: NodesCollection):
         self.nodes_collection = nodes_collection
-        self.sim_result = None
+        self.input_node_names = [
+            i.name for i in self.nodes_collection.get_input_nodes()
+        ]
+        self.kpi_node_names = [i.name for i in self.nodes_collection.get_kpi_nodes()]
+        self.sim_result = pd.DataFrame()
 
     def simulate(self) -> None:
-        """_summary_
+        self.simulate_variances()
+        self.update_calculations()
+
+    def simulate_variances(self) -> pd.DataFrame:
+        """
         Simulates all variations of the funnel by looping through all nodes and their value percentiles.
         Stores the results in self.sim_result.
 
@@ -29,9 +37,9 @@ class Funnel:
         if not any(node.is_kpi for node in self.nodes_collection):
             raise ValueError("No KPI node found in the funnel.")
 
-        inputs = [i.name for i in self.nodes_collection.get_input_nodes()]
+        inputs = self.input_node_names
         possible_values = [0, 1, 2]
-        kpis = [i.name for i in self.nodes_collection.get_kpi_nodes()]
+        kpis = self.kpi_node_names
 
         all_combinations = itertools.product(possible_values, repeat=len(inputs))
 
@@ -51,3 +59,21 @@ class Funnel:
             )
         results_df = pd.DataFrame(results)
         self.sim_result = results_df
+        return results_df
+
+    def update_calculations(self):
+        df = self.sim_result
+        kpi_cols = [f"{i}_{j}" for i in self.kpi_node_names for j in [0, 1, 2]]
+        calculations_df = pd.DataFrame(index=self.input_node_names, columns=kpi_cols)
+        for kpi in self.kpi_node_names:
+            for input in self.input_node_names:
+                for i in [0, 1, 2]:
+                    other_cols = [x for x in self.input_node_names if x != input]
+                    lookup = df[(df[input] == i) & ((df[other_cols] == 1).all(axis=1))]
+                    calculations_df.loc[input, f"{kpi}_{i}"] = lookup[kpi].values[0]
+        # Add swing (abs diff) and swing ^2
+        # What happen if there's a reverse relationship, so that the low-mid-high would lead to high-mid-low of kpi? interesting. I think in that case we should specify that input as "reverse" so that it's positive.
+        # e.g. churn, switch to survival.
+        # Because value base, most of the time if we are building the value funnel everything should more be positive correlation than negative.
+        self.calculations_result = calculations_df
+        return calculations_df
