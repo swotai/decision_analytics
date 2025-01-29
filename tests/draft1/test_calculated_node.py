6ad6702e083025a7a9cbae78232468527eb76eed
Dennis Tai <swotai@gmail.com>
2025-01-28 23:55:31 -0800

    Remove drafted ones.

--- tests/draft1/test_calculated_node.py
+++
@@ -1,8 +0,0 @@
-def test_calculated_node():
-    input_nodes = [InputNode(name="a", value=2), InputNode(name="b", value=3)]
-    node = CalculatedNode(name="sum", definition=lambda a, b: a + b, format_str="%d")
-    nodes = NodesCollection(input_nodes)
-    nodes.add_nodes([node])
-    nodes.refresh_nodes()
-
-    assert nodes.get_node("sum").value == 5
